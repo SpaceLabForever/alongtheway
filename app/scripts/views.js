98006c6935f804bddd99ed76c63c64855dd06940
David Anthony Puerto <me@davidpuerto.com>
2014-03-06 10:54:47 -0800

    Text size

--- app/scripts/views.js
+++ app/scripts/views.js
@@ -1,44 +1,4 @@
 'use strict';
-
-var renderListView = function(){
-  var catList = resultsObj.displayAllCategories();
-  var _T_list =  '';
-  for(var i in catList){
-    var listResults = resultsObj.getByCategory(catList[i]);
-    var spaceIt = catList[i].split("_").join(" ");
-    _T_list += '<li class="top-level ' + catList[i] + '"><input type="radio" name="filterList" id="' + catList[i] + '" />
-                <label class="button" for="' + catList[i] + '" data-counter="' + listResults.length + '">' + spaceIt + '</label>';
-    _T_list += '<div class="filter-level"><ul data-category="' + catList[i] + '" class="sub-menu">';
-    _.each(listResults, function(val){
-      var prettyName = val['name'].replace(/\s+/g,"");
-      _T_list += '<li><input class="switch" data-category="' + catList[i] + '" type="checkbox" id="' + catList[i] + "-"
-                  +  prettyName + '" value="' + prettyName + '" />' + '<label for="' + catList[i] + "-" + prettyName +'">'
-                  + val['name'] + '</label><button data-ref="' + val['reference']
-                  + '" class="more-info" type="button">i</button></li>';
-    });
-    _T_list += '</ul></div></li>';
-  }
-  return _T_list;
-};
-
-var testObj = {};
-
-
-var renderDetailView = function(place){
-  console.log(place);
-  testObj = place;
-  var _T_detail =
-  'Name' + place.name +
-  'Rating' + place.rating +
-  'Address' + place.formatted_address;
-  //return _T_detail;
-}
-
-function refreshView(id, viewOutput, stringify){
-  stringify ? (_spID(id).innerHTML = JSON.stringify(viewOutput)) : (_spID(id).innerHTML = viewOutput);
-  _invokeListListeners();
-}
-
 /******  Setting the useCapture to false and stopping the
 propagaiton of the event bubbling on the #map-canvas to ensure touch
 events on the map will not interfere with the app interface drawers and vice versa.  ******/
@@ -71,3 +31,41 @@ document.getElementById('open-left').addEventListener('click', function(){
     snapper.open('left');
   }
 });
+
+var renderListView = function(){
+  var catList = resultsObj.displayAllCategories();
+  var _T_list =  '';
+  for(var i in catList){
+    var listResults = resultsObj.getByCategory(catList[i]);
+    var spaceIt = catList[i].split("_").join(" ");
+    _T_list += '<li class="top-level ' + catList[i] + '"><input type="radio" name="filterList" id="' + catList[i] + '" />
+                <label class="button" for="' + catList[i] + '" data-counter="' + listResults.length + '">' + spaceIt + '</label>';
+    _T_list += '<div class="filter-level"><ul data-category="' + catList[i] + '" class="sub-menu">';
+    _.each(listResults, function(val){
+      var prettyName = val['name'].replace(/\s+/g,"");
+      _T_list += '<li><input class="switch" data-category="' + catList[i] + '" type="checkbox" id="' + catList[i] + "-"
+                  +  prettyName + '" value="' + prettyName + '" />' + '<label for="' + catList[i] + "-" + prettyName +'">'
+                  + val['name'] + '</label><button data-ref="' + val['reference']
+                  + '" class="more-info" type="button">i</button></li>';
+    });
+    _T_list += '</ul></div></li>';
+  }
+  return _T_list;
+};
+
+var testObj = {};
+
+var renderDetailView = function(place){
+  console.log(place);
+  testObj = place;
+  var _T_detail =
+  'Name' + place.name +
+  'Rating' + place.rating +
+  'Address' + place.formatted_address;
+  //return _T_detail;
+}
+
+function refreshView(id, viewOutput, stringify){
+  stringify ? (_spID(id).innerHTML = JSON.stringify(viewOutput)) : (_spID(id).innerHTML = viewOutput);
+  _invokeListListeners();
+}
